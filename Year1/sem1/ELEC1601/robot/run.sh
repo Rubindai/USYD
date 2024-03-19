9bd5b4527c917c5923fe1baaa24c2519cb932778
Rubin <rubindai2004@gmail.com>
2024-03-20 06:55:32 +1100

    Weekly update

--- Year1/sem1/ELEC1601/robot/run.sh
+++ Year1/sem1/ELEC1601/robot/run.sh
@@ -3,3 +3,4 @@ cd /home/rubin/USYD/First_year/elec1601/robot
 gcc wall.c formulas.c robot.c main.c -o main `sdl2-config --cflags --libs` -lm
 
 ./main
+
---
+++ Year1/sem2/INFO1113/week4/tut/Logger.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.File;
+
+
+public class Logger {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNextLine()){
+
+
+        }    }
+}
